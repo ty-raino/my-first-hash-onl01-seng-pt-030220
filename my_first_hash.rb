1d58ed45f0dc087af5c8ff7a5572ec7d449d29e9
Ty Raino <mstymetris@yahoo.com>
2020-03-13 22:10:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,7 +15,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-shipping_manifest[paintings]
+shipping_manifest["oil paintings"]
 end
 
 def adding
